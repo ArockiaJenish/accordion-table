da446d936658d7e07b34c57cae25b2310f213176
ArockiaJenishProdian <116620726+ArockiaJenishProdian@users.noreply.github.com>
2024-01-19 18:13:36 +0530

    Commiting for backup

--- jenish-app/src/app/app.component.ts
+++ jenish-app/src/app/app.component.ts
@@ -9,5 +9,22 @@ export class AppComponent {
   title = 'learn Angular';
   a = 12;
   b = 23;
-  c = this.a+this.b;
+  c = this.a + this.b;
+  height: any;
+
+  test(event: any){
+    console.log(event.target.value);
+    let val:string = event.target.value;
+    //console.log(/^[1-9][0-9]+\.?[0-9]{0,2}$/.test(val));
+    console.log(/^[0-9.]*$/.test(val));
+    if(!/^[0-9.]*$/.test(val)){
+      event.target.value = val.slice(0,-1);
+      console.log(this.height);
+    }
+    // if(!/^[1-9][0-9]+\.?[0-9]{0,2}$/.test(val)){
+    //   this.height = val.slice(0,-1);
+    //   console.log('if block');
+    //   console.log(this.height);
+    // }
+  }
 }
